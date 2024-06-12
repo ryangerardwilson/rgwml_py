cee63b8bb1afb732c10451143e1f4fad0b57399a
Ryan Gerard Wilson <ryangerardwilson@gmail.com>
2024-06-12 15:10:08 +0530

    sync

--- src/rgwml/docs.py
+++
@@ -1,7 +0,0 @@
-def show_docs():
-    """Prints out all methods in rgwml package with their signatures."""
-    methods = [
-        "query_to_df(preset_name, query_str)",
-    ]
-    for method in methods:
-        print(method)
