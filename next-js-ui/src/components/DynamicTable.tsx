0456eb8bae41581002a5ffef7e979e388a013e50
Ryan Gerard Wilson <ryangerardwilson@gmail.com>
2024-07-19 10:43:27 +0530

    sync

--- next-js-ui/src/components/DynamicTable.tsx
+++ next-js-ui/src/components/DynamicTable.tsx
@@ -88,159 +88,165 @@ const DynamicTable: React.FC<DynamicTableProps> = ({ apiHost, modal, columns, da
   };
 
 
-  return (
-    <div className="bg-black border border-yellow-100/30 rounded-lg text-yellow-100 p-4 text-sm">
-      <div className="mb-4">
-        {readRoutes.length > 0 && (
-          <div className="flex space-x-4">
-            {readRoutes.map((route) => (
-              <button
-                key={route}
-                className={`px-4 py-2 rounded ${activeTab === route ? 'border-t border-x border-yellow-100/50 rounded-lg' : 'bg-black text-yellow-100/50'}`}
-                onClick={() => setActiveTab(route)}
-              >
-                {route}
-              </button>
-            ))}
-          </div>
-        )}
-      </div>
-      <div className="flex justify-between mb-4">
-        <div className="flex items-center w-full">
-          <label className="flex items-center cursor-pointer relative">
-            <select
-              value={inputMode}
-              onChange={(e) => setInputMode(e.target.value as 'query' | 'search')}
-              className="bg-black border border-yellow-100/30 text-yellow-100 p-2 rounded-lg text-sm pr-8 appearance-none"
-            >
-              <option value="search">Search</option>
-              <option value="query">Query</option>
-            </select>
-            <span className="absolute right-3 pointer-events-none text-yellow-100/50 text-sm">▼</span>
-          </label>
-
-          {inputMode === 'query' ? (
-            <QueryInput
-              queryInput={queryInput}
-              handleQueryInputChange={handleQueryInputChange}
-              handleQueryKeyPress={handleQueryKeyPress}
-              queryError={queryError}
-            />
-          ) : (
-            <SearchInput
-              searchInput={searchInput}
-              handleSearchInputChange={handleSearchInputChange}
-              handleSearchKeyPress={handleSearchKeyPress}
-              searchError={searchError}
-            />
-          )}
+return (
+  <div className="bg-black border border-yellow-100/30 rounded-lg text-yellow-100 p-4 text-sm">
+    <div className="mb-4"> 
+      {readRoutes.length > 0 && (
+        <div className="flex space-x-4">
+          {readRoutes.map((route) => (
+            <button
+              key={route}
+              className={`px-4 py-2 rounded ${activeTab === route ? 'border-t border-x border-yellow-100/50 rounded-lg' : 'bg-black text-yellow-100/50'}`}
+              onClick={() => setActiveTab(route)}
+            > 
+              {route}
+            </button>
+          ))}
         </div>
-
-        <button
-          onClick={() => downloadCSV(data, modalConfig[modal]?.scopes.read, `${modal}_data`)}
-          className="bg-black border border-yellow-100/30 text-yellow-100/80 hover:bg-yellow-100/80 hover:text-black py-2 px-4 mr-4 rounded-lg text-sm"
-        >
-          CSV
-        </button>
-
-        {modalConfig[modal]?.scopes.create && (
-          <button
-            onClick={() => handleCreate(setCreateModalOpen)}
-            className="bg-black border border-yellow-100/30 text-yellow-100/80 hover:bg-yellow-100/80 hover:text-black py-2 px-4 rounded-lg text-sm"
-          >
-            Create
-          </button>
-        )}
-      </div>
-      <div className="overflow-x-auto h-96"> {/* Added height to make it scrollable */}
-        <table className="table-fixed min-w-full divide-y divide-yellow-100/30">
-          <thead className="bg-black sticky top-0"> {/* Added sticky and top classes */}
-            <tr>
-              <th className="px-3 py-3 text-left text-xs font-medium text-yellow-100 uppercase tracking-wider">Actions</th>
-              {modalConfig[modal]?.scopes.read.map((col: string, colIndex: number) => (
-                <th
-                  key={`col-${colIndex}`}
-                  className="px-3 py-3 text-left text-xs font-medium text-yellow-100 w-96"
-                >
-                  {col}
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((row, rowIndex) => (
-              <tr key={`row-${rowIndex}`} className="bg-black text-yellow-100/70 hover:bg-yellow-100/80 hover:text-black">
-                <td className="px-3 py-2 break-words text-sm text-gray-300 min-w-48">
-                  <button
-                    onClick={() => handleEdit(row, setEditRowData, setEditModalOpen)}
-                    className="bg-black border border-yellow-100/30 text-yellow-100/50 hover:bg-yellow-100/70 hover:text-black hover:border-black py-1 px-2 rounded-lg mr-2"
-                  >
-                    Edit
-                  </button>
-                  {modalConfig[modal]?.scopes.delete && (
-                    <button
-                      onClick={() => handleDelete(apiHost, modal, row[0], row[1], data, setData)}
-                      className="bg-black border border-yellow-100/30 text-yellow-100/50 hover:bg-yellow-100/70 hover:text-black hover:border-black py-1 px-2 rounded-lg mr-2"
-                    >
-                      Delete
-                    </button>
-                  )}
-                  <button
-                    onClick={() => copyToClipboard(row, modalConfig[modal]?.scopes.read)}
-                    className="bg-black border border-yellow-100/30 text-yellow-100/50 hover:bg-yellow-100/70 hover:text-black hover:border-black py-1 px-2 rounded-lg"
-                  >
-                    Copy
-                  </button>
-                </td>
-                {columnIndices.map((colIndex, cellIndex) => {
-                  const cellValue = row[colIndex];
-                  const truncatedValue = typeof cellValue === 'string' && cellValue.length > 250
-                                        ? `${cellValue.slice(0, 250)}...`
-                                        : cellValue;
-                  const isLongCellValue = typeof cellValue === 'string' && cellValue.length > 15;
-
-                  return (
-                    <td
-                      key={`cell-${rowIndex}-${cellIndex}`}
-                      className={`px-3 py-2 break-words text-sm ${isLongCellValue ? 'min-w-96' : ''}`}
-                    >
-                      {typeof truncatedValue === 'string' && isValidUrl(truncatedValue) ? (
-                        <button
-                          onClick={() => window.open(truncatedValue, '_blank')}
-                          className="bg-black border border-yellow-100/30 text-yellow-100/50 hover:bg-yellow-100/70 hover:text-black hover:border-black py-1 px-2 rounded-lg"
-                        >
-                          Open URL
-                        </button>
-                      ) : (
-                        typeof truncatedValue === 'string' && !isNaN(Date.parse(truncatedValue)) ? formatDateTime(truncatedValue) : truncatedValue
-                      )}
-                    </td>
-                  );
-                })}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      {isCreateModalOpen && (
-        <CreateModal
-          modalName={modal}
-          apiHost={apiHost}
-          columns={columns}
-          onClose={() => closeCreateModal(setCreateModalOpen)}
-        />
-      )}
-      {isEditModalOpen && editRowData && (
-        <EditModal
-          modalName={modal}
-          apiHost={apiHost}
-          columns={columns}
-          rowData={editRowData}
-          onClose={(updatedData) => closeEditModal(updatedData, columns, setData, setEditModalOpen)}
-        />
       )}
     </div>
-  );
+    <div className="flex justify-between mb-4">
+      <div className="flex items-center w-full">
+        <label className="flex items-center cursor-pointer relative">
+          <select
+            value={inputMode}
+            onChange={(e) => setInputMode(e.target.value as 'query' | 'search')}
+            className="bg-black border border-yellow-100/30 text-yellow-100 p-2 rounded-lg text-sm pr-8 appearance-none"
+          >
+            <option value="search">Search</option>
+            <option value="query">Query</option>
+          </select>
+          <span className="absolute right-3 pointer-events-none text-yellow-100/50 text-sm">▼</span>
+        </label>
+
+        {inputMode === 'query' ? (
+          <QueryInput
+            queryInput={queryInput}
+            handleQueryInputChange={handleQueryInputChange}
+            handleQueryKeyPress={handleQueryKeyPress}
+            queryError={queryError}
+          />
+        ) : (
+          <SearchInput
+            searchInput={searchInput}
+            handleSearchInputChange={handleSearchInputChange}
+            handleSearchKeyPress={handleSearchKeyPress}
+            searchError={searchError}
+          />
+        )}
+      </div>
+
+      <button
+        onClick={() => downloadCSV(data, modalConfig[modal]?.scopes.read, `${modal}_data`)}
+        className="bg-black border border-yellow-100/30 text-yellow-100/80 hover:bg-yellow-100/80 hover:text-black py-2 px-4 mr-4 rounded-lg text-sm"
+      >
+        CSV
+      </button>
+
+      {modalConfig[modal]?.scopes.create && (
+        <button
+          onClick={() => handleCreate(setCreateModalOpen)}
+          className="bg-black border border-yellow-100/30 text-yellow-100/80 hover:bg-yellow-100/80 hover:text-black py-2 px-4 rounded-lg text-sm"
+        >
+          Create
+        </button>
+      )}
+    </div>
+    <div className="flex justify-center items-center mb-1 text-center w-full">
+      <div className="text-yellow-100/50 text-sm italic text-center">
+        *** Showing {data.length} {data.length === 1 ? 'row' : 'rows'} ***
+      </div>
+    </div>
+    <div className="overflow-x-auto h-96"> {/* Added height to make it scrollable */}
+      <table className="table-fixed min-w-full divide-y divide-yellow-100/30">
+        <thead className="bg-black sticky top-0"> {/* Added sticky and top classes */}
+          <tr>
+            <th className="px-3 py-3 text-left text-xs font-medium text-yellow-100 uppercase tracking-wider">Actions</th>
+            {modalConfig[modal]?.scopes.read.map((col: string, colIndex: number) => (
+              <th
+                key={`col-${colIndex}`}
+                className="px-3 py-3 text-left text-xs font-medium text-yellow-100 w-96"
+              >
+                {col}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, rowIndex) => (
+            <tr key={`row-${rowIndex}`} className="bg-black text-yellow-100/70 hover:bg-yellow-100/80 hover:text-black">
+              <td className="px-3 py-2 break-words text-sm text-gray-300 min-w-64">
+                <button
+                  onClick={() => handleEdit(row, setEditRowData, setEditModalOpen)}
+                  className="bg-black border border-yellow-100/30 text-yellow-100/50 hover:bg-yellow-100/70 hover:text-black hover:border-black py-1 px-2 rounded-lg mr-2"
+                >
+                  Edit
+                </button>
+                {modalConfig[modal]?.scopes.delete && (
+                  <button
+                    onClick={() => handleDelete(apiHost, modal, row[0], row[1], data, setData)}
+                    className="bg-black border border-yellow-100/30 text-yellow-100/50 hover:bg-yellow-100/70 hover:text-black hover:border-black py-1 px-2 rounded-lg mr-2"
+                  >
+                    Delete
+                  </button>
+                )}
+                <button
+                  onClick={() => copyToClipboard(row, modalConfig[modal]?.scopes.read)}
+                  className="bg-black border border-yellow-100/30 text-yellow-100/50 hover:bg-yellow-100/70 hover:text-black hover:border-black py-1 px-2 rounded-lg"
+                >
+                  Copy
+                </button>
+              </td>
+              {columnIndices.map((colIndex, cellIndex) => {
+                const cellValue = row[colIndex];
+                const truncatedValue = typeof cellValue === 'string' && cellValue.length > 250
+                                      ? `${cellValue.slice(0, 250)}...`
+                                      : cellValue;
+                const isLongCellValue = typeof cellValue === 'string' && cellValue.length > 15;
+
+                return (
+                  <td
+                    key={`cell-${rowIndex}-${cellIndex}`}
+                    className={`px-3 py-2 break-words text-sm ${isLongCellValue ? 'min-w-96' : ''}`}
+                  >
+                    {typeof truncatedValue === 'string' && isValidUrl(truncatedValue) ? (
+                      <button
+                        onClick={() => window.open(truncatedValue, '_blank')}
+                        className="bg-black border border-yellow-100/30 text-yellow-100/50 hover:bg-yellow-100/70 hover:text-black hover:border-black py-1 px-2 rounded-lg"
+                      >
+                        Open URL
+                      </button>
+                    ) : (
+                      typeof truncatedValue === 'string' && !isNaN(Date.parse(truncatedValue)) ? formatDateTime(truncatedValue) : truncatedValue
+                    )}
+                  </td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+    {isCreateModalOpen && (
+      <CreateModal
+        modalName={modal}
+        apiHost={apiHost}
+        columns={columns}
+        onClose={() => closeCreateModal(setCreateModalOpen)}
+      />
+    )}
+    {isEditModalOpen && editRowData && (
+      <EditModal
+        modalName={modal}
+        apiHost={apiHost}
+        columns={columns}
+        rowData={editRowData}
+        onClose={(updatedData) => closeEditModal(updatedData, columns, setData, setEditModalOpen)}
+      />
+    )}
+  </div>
+);
+
 
 };
 
